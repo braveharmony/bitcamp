579d0340d54e815c3665e7af12b701608ca05be0
seungwon <jang06792000@gmail.com>
2023-03-10 16:50:12 +0900

    ain

---
+++ keras/keras23_Scaler01_boston.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_boston
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+datasets = load_boston()
+x=datasets['data']
+y=datasets['target']
+
+print(x)

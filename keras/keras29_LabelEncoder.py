65751285241712957f2bf3923092a4ef360117d9
seungwon <jang06792000@gmail.com>
2023-03-15 14:32:22 +0900

    ain

--- keras/keras29_LabelEncoder.py
+++ keras/keras29_LabelEncoder.py
@@ -2,17 +2,19 @@
 import random
 import numpy as np
 import tensorflow as tf
-seed=4
+seed=24
 random.seed(seed)
 np.random.seed(seed)
 tf.random.set_seed(seed)
-
+tc=24
 
 # 1. 데이터
 import pandas as pd
+import pandas_profiling as pp
 path="./_data/dacon_wine/"
 path_save='./_save/dacon_wine/'
 df=pd.read_csv(path+'train.csv',index_col=0)
+# df.profile_report().to_file('./pr_report.html')
 dft=pd.read_csv(path+'test.csv',index_col=0)
 dfs=pd.read_csv(path+'sample_submission.csv')
 df=df.dropna()
@@ -29,10 +31,9 @@ x[df.columns[-1]]=le.transform(x[df.columns[-1]])
 dft[df.columns[-1]]=le.transform(dft[df.columns[-1]])
 # ros = RandomOverSampler()
 # x, y = ros.fit_resample(x, y)
-# print(np.unique(x['type'],return_counts=True))
+print(np.unique(x['type'],return_counts=True))
 x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.8,random_state=seed,stratify=y)
-x_train=x
-y_train=y
+# x_train=x;y_train=y
 scaler=RobustScaler()
 x_train=scaler.fit_transform(x_train)
 x_test=scaler.transform(x_test)
@@ -43,18 +44,18 @@ from tensorflow.python.keras.models import Sequential,Model
 from tensorflow.python.keras.layers import Dense,Input,LeakyReLU,Dropout
 # 2. model build
 input1=Input(shape=(x.shape[1],))
-layer = Dense(32,activation=LeakyReLU(0.35))(input1)
-layer = Dropout(0.25)(layer)
-layer = Dense(32,activation=LeakyReLU(0.35))(layer)
-layer = Dropout(0.25)(layer)
-layer = Dense(32,activation=LeakyReLU(0.35))(layer)
-layer = Dropout(0.25)(layer)
-layer = Dense(32,activation=LeakyReLU(0.35))(layer)
-layer = Dropout(0.25)(layer)
-layer = Dense(32,activation=LeakyReLU(0.55))(layer)
+layer = Dense(32,activation='relu')(input1)
+layer = Dropout(0.2)(layer)
+layer = Dense(32,activation='relu')(layer)
+layer = Dropout(0.2)(layer)
+layer = Dense(16,activation='relu')(layer)
+layer = Dropout(0.2)(layer)
+layer = Dense(16,activation='relu')(layer)
+layer = Dropout(0.2)(layer)
+layer = Dense(32,activation='relu')(layer)
 layer = Dropout(0.1)(layer)
-layer = Dense(32,activation=LeakyReLU(0.55))(layer)
-layer = Dropout(0.1)(layer)
+layer = Dense(32,activation='relu')(layer)
+# layer = Dropout(0.1)(layer)
 # layer = Dense(32,activation=LeakyReLU(0.85))(layer)
 # layer = Dropout(0.1)(layer)
 # layer = Dense(32,activation=LeakyReLU(0.85))(layer)
@@ -62,13 +63,13 @@ layer = Dropout(0.1)(layer)
 layer = Dense(y.shape[1],activation='softmax')(layer)
 model=Model(inputs=input1,outputs=layer)
 # model.summary()
-
+model.save(f'./_save/dacon_wine/03_15/model{tc}.h5')
 # 3. 컴파일,훈련
 from tensorflow.python.keras.callbacks import EarlyStopping
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics='acc')
-hist=model.fit(x_train,y_train,batch_size=len(x_train),epochs=10000
+hist=model.fit(x_train,y_train,batch_size=len(x_train)//100,epochs=10000
           ,validation_split=0.2,verbose=True
-          ,callbacks=EarlyStopping(monitor='val_loss',mode='min',patience=200,restore_best_weights=True,verbose=True))
+          ,callbacks=EarlyStopping(monitor='val_loss',mode='min',patience=400,restore_best_weights=True,verbose=True))
 
 # # 4. 평가, 예측
 from sklearn.metrics import accuracy_score
@@ -76,8 +77,8 @@ loss=model.evaluate(x_test,y_test)
 print(f'loss : {loss}\naccuarcy : {accuracy_score(np.argmax(y_test,axis=1)+3,np.argmax(model.predict(x_test),axis=1)+3)}')
 
 dfs[df.columns[0]]=np.argmax(model.predict(dft),axis=1)+3
-dfs.to_csv('./_save/dacon_wine/03_15/forsub.csv',index=False)
-model.save('./_save/dacon_wine/03_15/forsub.h5')
+dfs.to_csv(f'./_save/dacon_wine/03_15/forsub{tc}.csv',index=False)
+model.save(f'./_save/dacon_wine/03_15/forsub{tc}.h5')
 
 import matplotlib.pyplot as plt
 plt.subplot(1,2,1)

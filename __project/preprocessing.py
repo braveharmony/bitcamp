cee5b4a44cebf23e356e5c75f812e043b9f7a1e6
braveharmony <jang06792000@gmail.com>
2023-04-07 18:11:06 +0900

    ain

--- __project/preprocessing.py
+++ __project/preprocessing.py
@@ -1,20 +1,45 @@
 import subprocess
 import numpy as np
+
+#######################################################
+# 52명의 캐릭터와 7종류의 곡중 어느걸 쓸것인지
+# sampling_rate는 몇으로 리셈플링 할것인지 아래를 통해 결정
+#######################################################
+sampling_rate=np.array([8000])
+idolnum=[1,2,3,4]
+musicindex=[0,1,2,3,4,5,6]
+
+# # 특정 음악만 뽑고 싶으면
+# audio_signal=np.load(f"d:/study_data/_project/_data/audio_signal/{music}.npy")
+# r_fftx=np.load(f"d:/study_data/_project/_data/r_fftx/{music}r_fftx.npy")
+# i_fftx=np.load(f"d:/study_data/_project/_data/i_fftx/{music}i_fftx.npy")
+# mfcc=np.load(f"d:/study_data/_project/_data/mfcc/{music}mfcc.npy")
+# sr=np.load(f"d:/study_data/_project/_data/sr/{music}sr.npy")
+# y=np.load(f"d:/study_data/_project/_data/ys/{music}ys.npy")
+
+
+# # 모든 데이터를 뽑고 싶으면
+# audio_signal=np.load(f"d:/study_data/_project/_data/audio_signal.npy")
+# r_fftx=np.load(f"d:/study_data/_project/_data/r_fftx.npy")
+# i_fftx=np.load(f"d:/study_data/_project/_data/i_fftx.npy")
+# mfcc=np.load(f"d:/study_data/_project/_data/mfcc.npy")
+# sr=np.load(f"d:/study_data/_project/_data/sr.npy")
+# y=np.load(f"d:/study_data/_project/_data/ys.npy")
+
+########################################################################################################
+
+np.save(f"d:/study_data/_project/_data/sr.npy", np.array(list(sampling_rate)))
+np.save(f"d:/study_data/_project/_data/idolnum.npy", np.array(list(idolnum)))
+np.save(f"d:/study_data/_project/_data/musicindex.npy", np.array(list(musicindex)))
+
+########################################################################################################
 # 실행할 파일 목록
 files_to_run = ["tonumpy.py", "numpy_to_fft.py", "numpy_to_MFCC.py", "numpy_concate.py", "last_concate.py"]
-
-sampling_rate=np.array([8000])
-idolnum=np.array(range(1,53))
-musicindex=np.array([0,1,2,3,4,5,6])
-
-np.save(f"d:/study_data/_project/_data/sr.npy", sampling_rate)
-np.save(f"d:/study_data/_project/_data/idolnum.npy", idolnum)
-np.save(f"d:/study_data/_project/_data/musicindex.npy", musicindex)
-
+folder="./__project/preprocessing/"
 
 for file in files_to_run:
     # 파일을 실행
-    file=f'./__project/preprocessing/{file}'
+    file=f'{folder}{file}'
     result = subprocess.run(["python", file], stdout=None, stderr=subprocess.PIPE)
 
     if result.returncode != 0:

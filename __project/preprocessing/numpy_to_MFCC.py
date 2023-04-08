8c023e8774874cd8d1c85d1583a262c517fbc66e
starrymelody <82393469+braveharmony@users.noreply.github.com>
2023-04-08 23:08:56 +0900

    Update numpy_to_MFCC.py

--- __project/preprocessing/numpy_to_MFCC.py
+++ __project/preprocessing/numpy_to_MFCC.py
@@ -2,21 +2,25 @@ import numpy as np
 import librosa
 import time
 
-starttime=time.time()
-# mp3 파일 로드
-music_sample=('Crossing!','Dreaming!','Flyers!!!','GlowMap','Harmony4You','Rainbow','UNION!!')
-index=np.load(f"d:/study_data/_project/_data/musicindex.npy")
-musics=(music_sample[i] for i in index)
-idolnum=np.load(f"d:/study_data/_project/_data/idolnum.npy")
+def file_path(file_extension, filenum, folder=''):
+    return f"d:/study_data/_project/_data/{folder}/{filenum}.{file_extension}"
+
+index = np.load(file_path("npy", "musicindex"))
+musics = ("Crossing!", "Dreaming!", "Flyers!!!", "GlowMap", "Harmony4You", "Rainbow", "UNION!!")
+idolnum = np.load(file_path("npy", "idolnum"))
+
+starttime = time.time()
+
 for folder in musics:
     for filenum in idolnum:
-        audio_signal=np.load(f"d:/study_data/_project/_data/{folder}/{filenum}.npy")
-        sample_rate=np.load(f"d:/study_data/_project/_data/{folder}/{filenum}sr.npy")[0]
-        freq=np.fft.fftfreq(len(audio_signal),1/sample_rate)
-        mfcc=np.array([librosa.feature.mfcc(y=audio_signal[i], sr=sample_rate, n_mfcc=20
-                            , n_fft=sample_rate//50, hop_length=sample_rate//200,center=True,n_mels=20,win_length=sample_rate//50).T for i in range(len(audio_signal))])
-        np.save(f"d:/study_data/_project/_data/{folder}/{filenum}mfcc.npy", mfcc.astype(np.float32))
-        print(f'{folder}폴더 {filenum}번 save완료! sr: {sample_rate}')
-        print(f'runtime : {time.time()-starttime}')
+        audio_signal = np.load(file_path("npy", filenum, folder))
+        sample_rate = np.load(file_path("npy", f"{filenum}sr", folder))[0]
+        freq = np.fft.fftfreq(len(audio_signal), 1 / sample_rate)
+        mfcc = np.array([librosa.feature.mfcc(y=audio_signal[i], sr=sample_rate, n_mfcc=20, n_fft=sample_rate // 50, 
+                                              hop_length=sample_rate // 200, center=True, n_mels=20,
+                                              win_length=sample_rate // 50).T for i in range(len(audio_signal))])
+        np.save(file_path("npy", f"{filenum}mfcc", folder), mfcc.astype(np.float32))
+        
+        print(f"{folder}폴더 {filenum}번 save완료! sr: {sample_rate}")
+        print(f"runtime : {time.time()-starttime}")
         print(mfcc.shape)
-    

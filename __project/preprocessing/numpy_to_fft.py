feaae96b0738d68768895244ca6ea2ef2da3a8e7
starrymelody <82393469+braveharmony@users.noreply.github.com>
2023-04-08 23:20:46 +0900

    Update numpy_to_fft.py

--- __project/preprocessing/numpy_to_fft.py
+++ __project/preprocessing/numpy_to_fft.py
@@ -2,24 +2,29 @@ import numpy as np
 import librosa
 import time
 
-starttime=time.time()
-# mp3 파일 로드
-music_sample=('Crossing!','Dreaming!','Flyers!!!','GlowMap','Harmony4You','Rainbow','UNION!!')
-index=np.load(f"d:/study_data/_project/_data/musicindex.npy")
-musics=(music_sample[i] for i in index)
-idolnum=np.load(f"d:/study_data/_project/_data/idolnum.npy")
+def file_path(file_extension, filenum, folder=''):
+    return f"d:/study_data/_project/_data/{folder}/{filenum}.{file_extension}"
+
+index = np.load(file_path("npy", "musicindex"))
+musics = ("Crossing!", "Dreaming!", "Flyers!!!", "GlowMap", "Harmony4You", "Rainbow", "UNION!!")
+idolnum = np.load(file_path("npy", "idolnum"))
+
+starttime = time.time()
+
 for folder in musics:
     for filenum in idolnum:
-        audio_signal=np.load(f"d:/study_data/_project/_data/{folder}/{filenum}.npy")
-        sample_rate=np.load(f"d:/study_data/_project/_data/{folder}/{filenum}sr.npy")[0]
-        freq=np.fft.fftfreq(len(audio_signal),1/sample_rate)
-        fftx=np.array([np.fft.fft(audio_signal[i])for i in range(len(audio_signal))])
-        r_fftx=np.real(fftx)/np.sqrt(len(audio_signal))
-        r_fftx=r_fftx.reshape(*r_fftx.shape,1)
-        i_fftx=np.imag(fftx)/np.sqrt(len(audio_signal))
-        i_fftx=i_fftx.reshape(*i_fftx.shape,1)
-        np.save(f"d:/study_data/_project/_data/{folder}/{filenum}r_fftx.npy", r_fftx[:,:audio_signal.shape[1]//2].astype(np.float32))
-        np.save(f"d:/study_data/_project/_data/{folder}/{filenum}i_fftx.npy",i_fftx[:,:audio_signal.shape[1]//2].astype(np.float32))
-        print(f'{folder}폴더 {filenum}번 save완료! sr: {sample_rate}')
-        print(f'runtime : {time.time()-starttime}')
-    
+        audio_signal = np.load(file_path("npy", filenum, folder))
+        sample_rate = np.load(file_path("npy", f"{filenum}sr", folder))[0]
+        
+        freq = np.fft.fftfreq(len(audio_signal), 1 / sample_rate)
+        fftx = np.fft.fft(audio_signal)
+        
+        r_fftx = np.real(fftx) / np.sqrt(len(audio_signal))
+        i_fftx = np.imag(fftx) / np.sqrt(len(audio_signal))
+        r_fftx = r_fftx[:, :audio_signal.shape[1] // 2].reshape(*r_fftx.shape, 1)
+        i_fftx = i_fftx[:, :audio_signal.shape[1] // 2].reshape(*i_fftx.shape, 1)
+        
+        np.save(file_path("npy", f"{filenum}r_fftx", folder), r_fftx.astype(np.float32))
+        np.save(file_path("npy", f"{filenum}i_fftx", folder), i_fftx.astype(np.float32))
+        print(f"{folder}폴더 {filenum}번 save완료! sr: {sample_rate}")
+        print(f"runtime: {time.time() - starttime}")
